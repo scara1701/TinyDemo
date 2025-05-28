f6aa41e12ac6a0625a1b185f2c3b476db7982dd1
Gwen Demulder <31825690+scara1701@users.noreply.github.com>
2025-05-28 20:19:05 +0200

    Binding custom control maui works

--- TinyDemo.MauiClient/Controls/NumberControl.xaml.cs
+++ TinyDemo.MauiClient/Controls/NumberControl.xaml.cs
@@ -1,20 +1,30 @@
+
+using System.ComponentModel;
+
 namespace TinyDemo.MauiClient.Controls;
 
 public partial class NumberControl : ContentView
 {
-
+    //https://learn.microsoft.com/en-us/dotnet/maui/user-interface/controls/contentview?view=net-maui-9.0
     public static readonly BindableProperty NumberProperty =
-    BindableProperty.Create(nameof(Number), typeof(int), typeof(NumberControl), default(int));
+    BindableProperty.Create(nameof(Number), typeof(int), typeof(NumberControl), 0, propertyChanged: OnNumberChanged);
+
+    private static void OnNumberChanged(BindableObject bindable, object oldValue, object newValue)
+    {
+        var control = (NumberControl)bindable;
+    }
 
     public int Number
     {
         get => (int)GetValue(NumberProperty);
-        set => SetValue(NumberProperty, value);
+        set {
+            SetValue(NumberProperty, value);
+            OnPropertyChanged(nameof(Number));
+        }
     }
-
 
     public NumberControl()
 	{
 		InitializeComponent();
-	}
+    }
 }
---
+++ TinyDemo.MauiClient/Converters/IntToStringConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TinyDemo.MauiClient.Converters
+{
+    public class IntToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value?.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return int.TryParse(value?.ToString(), out int result) ? result : 0;
+        }
+    }
+
+
+}
